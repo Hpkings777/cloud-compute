f6d089932ef5106946cb0caf71bb213d721b9cf4
root <root@localhost.localdomain>
2026-05-14 15:09:27 +0000

    Fix scene import paths

--- src/OhmsLaw.tsx
+++ src/OhmsLaw.tsx
@@ -1,11 +1,11 @@
 import { AbsoluteFill, useCurrentFrame, interpolate, Sequence } from 'remotion';
-import { Scene1 } from '../scenes/Scene1';
-import { Scene2 } from '../scenes/Scene2';
-import { Scene3 } from '../scenes/Scene3';
-import { Scene4 } from '../scenes/Scene4';
-import { Scene5 } from '../scenes/Scene5';
-import { Scene6 } from '../scenes/Scene6';
-import { Scene7 } from '../scenes/Scene7';
+import { Scene1 } from './scenes/Scene1';
+import { Scene2 } from './scenes/Scene2';
+import { Scene3 } from './scenes/Scene3';
+import { Scene4 } from './scenes/Scene4';
+import { Scene5 } from './scenes/Scene5';
+import { Scene6 } from './scenes/Scene6';
+import { Scene7 } from './scenes/Scene7';
 
 const DURATION = 3600;
 
@@ -18,43 +18,43 @@ export const OhmsLawAnimation: React.FC = () => {
 
   return (
     <AbsoluteFill style={{ background: '#0a0e1a' }}>
-      <Sequence from={0} duration={300}>
+      <Sequence from={0} name="Scene1">
         <div style={{ opacity: transitionOpacity(0, 300) }}>
           <Scene1 />
         </div>
       </Sequence>
       
-      <Sequence from={300} duration={900}>
+      <Sequence from={300} name="Scene2">
         <div style={{ opacity: transitionOpacity(300, 1200) }}>
           <Scene2 />
         </div>
       </Sequence>
       
-      <Sequence from={1200} duration={450}>
+      <Sequence from={1200} name="Scene3">
         <div style={{ opacity: transitionOpacity(1200, 1650) }}>
           <Scene3 />
         </div>
       </Sequence>
       
-      <Sequence from={1650} duration={600}>
+      <Sequence from={1650} name="Scene4">
         <div style={{ opacity: transitionOpacity(1650, 2250) }}>
           <Scene4 />
         </div>
       </Sequence>
       
-      <Sequence from={2250} duration={600}>
+      <Sequence from={2250} name="Scene5">
         <div style={{ opacity: transitionOpacity(2250, 2850) }}>
           <Scene5 />
         </div>
       </Sequence>
       
-      <Sequence from={2850} duration={600}>
+      <Sequence from={2850} name="Scene6">
         <div style={{ opacity: transitionOpacity(2850, 3450) }}>
           <Scene6 />
         </div>
       </Sequence>
       
-      <Sequence from={3450} duration={150}>
+      <Sequence from={3450} name="Scene7">
         <div style={{ opacity: transitionOpacity(3450, 3600) }}>
           <Scene7 />
         </div>
